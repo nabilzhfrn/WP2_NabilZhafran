e15993fdd2539fef63c8ee2dab2abcbff1106c9a
nabilzhfrn <91109557+nabilzhfrn@users.noreply.github.com>
2021-10-10 14:39:28 +0700

    Pertemuan - 4

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Shinichi</li>
+        <li>Nama Belakang : Kudo</li>
+        <li>Nama Lengkap : Shinichi Kudo </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Bekasi City of the Light, Olympus.st, No.15B</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li> Bekasi City of the Dark</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Mendengarkan Lagu</li>
+    </ul>
+</section>
